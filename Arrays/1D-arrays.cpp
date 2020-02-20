e16e63d336c5ab19c521ae92df1b413fa937fad8
Roman-Efremov <59253524+Roman-Efremov@users.noreply.github.com>
2020-02-20 20:33:01 +0300

    Add files via upload

--- Arrays/1D-arrays.cpp
+++ Arrays/1D-arrays.cpp
@@ -1,136 +1,113 @@
 ﻿//одномерные массивы
 #include <iostream>
+#include<ctime>
+
 using namespace std;
 
-int* giveMemory(int N)
+void makeArr(int N, int*& p)
 {
-	int* A;
-	try {
-		A = new int[N];
-		A = { 0 };
-		return A;
-	}
-	catch (...) {
-		cout << "error";
-	}
+	p = new (nothrow) int[N];
+	if (!p) exit(404);
 }
 
-void freeMemory(int A[]) 
+void freeMemory(int*& A)
 {
 	delete[] A;
+	A = nullptr;
 }
 
-void initRandomArray(int A[], const int N)
+void initRandomArray(int* beg, int* end, const int M, const int m)
 {
-	for (int i = 0; i < N; ++i)
-	{
-		int k = rand() % 10 - 1;
-		A[i] = k;
-	}
+	srand(time(0));
+	int d = M - m + 1;
+	for (int* p = beg; p < end; ++p)
+		 *p = rand() % d + m;
 }
 
-void printArray(int A[], const int N)
+void printArray(int *beg, int *end)
 {
-	for (int i = 0; i < N; ++i)
+	for (int *p = beg; p < end; ++p)
 	{
-		cout << A[i] << " ";
+		cout << *p << " ";
 	}
 	cout << '\n';
 }
 
-void howManySomething(int A[], const int n, const int N)
+void howManySomething(int* beg, int* end , const int n)
 {
 	int s = 0;
-	for (int i = 0; i < N; ++i)
+	for (int* p = beg; p < end; ++p)
 	{
-		if(A[i] == n) ++s;
+		if( *p == n) ++s;
 	}
 	cout << "The number of " << n << " = " << s <<'\n';
 }
 
-void howManyMaxMin(int A[], const int N)
+void howManyMaxMin(int* beg, int* end)
 {
-	int nmin = 0, nmax = 0, max = A[0], min = A[0];
-	for (int i = 0; i < N; ++i)
+	int nmin = 0, nmax = 0, max = *beg, min = *beg;
+	for (int* p = beg; p < end; ++p)
 	{
-		if (A[i] > max)
+		if (*p > max)
 		{
-			max = A[i];
+			max = *p;
 			nmax = 0;
 		}
-		if (A[i] < min)
+		if (*p < min)
 		{
-			min = A[i];
+			min = *p;
 			nmin = 0;
 		}
-		if (A[i] == max)
+		if (*p == max)
 			++nmax;
-		if (A[i] == min)
+		if (*p == min)
 			++nmin;
 	}
 	cout << "max = " << max << '\n' << "min = " << min << '\n' << "Number of max = " << nmax << '\n' << "Number of min = " << nmin << '\n';
 }
 
-int findMin(int A[], const int N)
+int findMin(int* beg, int* end)
 {
-	int  min = A[0];
-	for (int i = 0; i < N; ++i)
+	int  min = *beg;
+	for (int* p = beg; p < end; ++p)
 	{
-		if(A[i] < min) min = A[i];
+		if(*p < min) min = *p;
 	}
 	return min;
 }
 
-int findMax(int A[], const int N)
+int findMax(int* beg, int* end)
 {
-	int  max = A[0];
-	for (int i = 0; i < N; ++i)
+	int  max = *beg;
+	for (int* p = beg; p < end; ++p)
 	{
-		if(A[i] > max) max = A[i];
+		if(*p > max) max = *p;
 	}
 	return max;
 }
 
-void rightPermutation(int A[], const int N)
+void leftPermutation(int* beg, int* end)
 {
-	int t;
-	for (int i = 1; i < N; ++i)
+	for (int* p = beg; p < end - 1; ++p)
 	{
-		t = A[N - i];
-		A[N - i] = A[N - i - 1];
-		A[N - i - 1] = t;
+		swap(*p, *(p + 1));
 	}
 }
 
-void leftPermutation(int A[], const int N)
+void rightPermutation(int* beg, int* end)
 {
-	int t;
-	for (int i = 0; i < N - 1; ++i)
+	for (int* p = end - 1; p > beg; --p)
 	{
-		t = A[i];
-		A[i] = A[i + 1];
-		A[i + 1] = t;
+		swap(*p, *(p - 1));
 	}
 }
 
-void sortArr(int A[],const int N)
-{
-	int i = 0, j = 1;
-	int *B=new int[N];
-	for (int m = 0; m < N; ++m)
-	{
-		if (A[m] > 0) { B[i] = A[m]; ++i; }
-		if (A[m] < 0) { B[N - j] = A[m]; ++j; }
-	}
-	for (int q = 0; q < N; ++q)
-		A[q] = B[q];
-}
+//sortArr is coming soon
 
-void reverse(int A[], const int N)
+void reverse(int *beg, int *end)
 {
-	int* B = new int[N];
-	for (int i = 0; i < N; ++i)
-		B[N - i - 1] = A[i];
-	for (int i = 0; i < N; ++i)
-		A[i] = B[i];
+	for (int *p=beg, int*q=end-1; p < q; ++p,--q)
+	{
+		swap(*p, *q);
+	}
 }
---
+++ Arrays/1D-arrays.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+void makeArr(int, int*&);
+void freeMemory(int*&);
+void initRandomArray(int*, int*, const int, const int);
+void printArray(int*, int*);
+void printArray(int*, int*);
+void howManySomething(int*, int*, const int);
+void howManyMaxMin(int*, int*);
+int findMin(int*, int*);
+int findMax(int*, int* );
+void rightPermutation(int *, int* );
+void leftPermutation(int *, int* );
+void reverse(int*, int*);
