5e35674dd6a647bcf54f8b3edcffe1fb4a282372
Roman-Efremov <59253524+Roman-Efremov@users.noreply.github.com>
2020-02-26 22:24:21 +0300

    Update 1D-arrays.h

--- Arrays/1D-arrays.h
+++ Arrays/1D-arrays.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 void makeArr(int*&, int);
 void freeMemory(int*&);
-void initRandomArray(int*, int*, const int, const int);
+void initRandomArray(int*, int*, int m=-10, int M=10);
 void printArray(int*, int*);
 void printArray(int*, int*);
 void howManySomething(int*, int*, const int);
@@ -10,4 +10,6 @@ int findMin(int*, int*);
 int findMax(int*, int* );
 void rightPermutation(int *, int* );
 void leftPermutation(int *, int* );
+void bubbleSort(int* beg, int* end);
+void quicksort(int* beg, int* end);
 void reverse(int*, int*);
