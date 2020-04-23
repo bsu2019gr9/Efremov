efc132c6f8d6270c36f947d4846b64a43747a6c9
Roman-Efremov <59253524+Roman-Efremov@users.noreply.github.com>
2020-04-23 09:52:10 +0300

    Add files via upload

---
+++ 5-2/EngMoney.cpp
@@ -0,0 +1,140 @@
+﻿#include <iostream>
+#include "EngMoney.h"
+using namespace std;
+
+//Класс английские деньги(1 фунт стерлингов = 20 шиллингов ,1 шиллинг = 12 пенсов).
+void normalize(EngMoney& m)
+{
+	m.pound += m.shill / 20 + m.pence / 240;
+	m.shill = m.shill % 20 + (m.pence / 12) % 20;
+	m.pence %= 12;
+	if (m.shill >= 20 || m.shill <= 20)
+	{
+		m.pound += m.shill / 20;
+		m.shill %= 20;
+	}
+}
+
+EngMoney::EngMoney() :
+	pound(0),
+	shill(0),
+	pence(0)
+{
+	//cout << "no params constructor working \n";
+}
+
+EngMoney::EngMoney(int a, int b, int c) :
+	pound(a),
+	shill(b),
+	pence(c)
+{
+	normalize(*this);
+	//cout << "constructor working for " << this->pound << ' ' << this->shill << ' ' << this->pence << "\n";
+}
+
+EngMoney::~EngMoney()
+{
+	//cout << "destructor working for...\n"; 
+}
+
+EngMoney::EngMoney(const EngMoney& m) :
+	pound(m.pound),
+	shill(m.shill),
+	pence(m.pence)
+{
+	//cout << "copy constructor working for" << this->pound << ' ' << this->shill << ' ' << this->pence << "\n";
+}
+
+void EngMoney::operator=(const EngMoney& m)
+{
+	pound = m.pound;
+	shill = m.shill;
+	pence = m.pence;
+}
+
+EngMoney EngMoney::operator+(const EngMoney& m) const
+{
+	return EngMoney(pound + m.pound, shill + m.shill, pence + m.pence);
+}
+
+EngMoney EngMoney::operator-(const EngMoney& m) const
+{
+	return EngMoney(pound - m.pound, shill - m.shill, pence - m.pence);
+}
+
+EngMoney EngMoney::operator*(const int n) const
+{
+	return EngMoney(pound * n, shill * n, pence * n);
+}
+
+EngMoney EngMoney::operator+=(const EngMoney& m)
+{
+	pound += m.pound;
+	shill += m.shill;
+	pence += m.pence;
+	normalize(*this);
+	return EngMoney(*this);
+}
+
+EngMoney EngMoney::operator-=(const EngMoney& m)
+{
+	pound -= m.pound;
+	shill -= m.shill;
+	pence -= m.pence;
+	normalize(*this);
+	return EngMoney(*this);
+}
+
+EngMoney EngMoney::operator*=(const int n)
+{
+	pound *= n;
+	shill *= n;
+	pence *= n;
+	normalize(*this);
+	return EngMoney(*this);
+}
+
+bool EngMoney::operator==(const EngMoney& m) const
+{
+	return(pound == m.pound && shill == m.shill && pence == m.pence);
+}
+
+bool EngMoney::operator!=(const EngMoney& m) const
+{
+	return(pound != m.pound || shill != m.shill || pence != m.pence);
+}
+
+bool EngMoney::operator>(const EngMoney& m) const
+{
+	return(pound * 240 + shill * 12 + pence > m.pound * 240 + m.shill * 12 + m.pence);
+}
+
+bool EngMoney::operator>=(const EngMoney& m) const
+{
+	return(pound * 240 + shill * 12 + pence >= m.pound * 240 + m.shill * 12 + m.pence);
+}
+
+bool EngMoney::operator<(const EngMoney& m) const
+{
+	return(pound * 240 + shill * 12 + pence < m.pound * 240 + m.shill * 12 + m.pence);
+}
+
+bool EngMoney::operator<=(const EngMoney& m) const
+{
+	return(pound * 240 + shill * 12 + pence <= m.pound * 240 + m.shill * 12 + m.pence);
+}
+
+ostream& operator<<(ostream& out, const EngMoney& m)
+{
+	out << m.pound << " po " << m.shill << " sh " << m.pence <<" pe";
+	return out;
+}
+
+istream& operator>>(istream& in, EngMoney& m)
+{
+	in >> m.pound;
+	in >> m.shill;
+	in >> m.pence;
+	normalize(m);
+	return in;
+}
---
+++ 5-2/EngMoney.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+//Класс английские деньги(1 фунт стерлингов = 20 шиллингов ,1 шиллинг = 12 пенсов).
+class EngMoney
+{
+private:
+	int pound;
+	int shill;
+	int pence;
+
+	friend void normalize(EngMoney& m);
+public:
+	EngMoney();
+	EngMoney(int a, int b = 0, int c = 0);
+	~EngMoney();
+	EngMoney(const EngMoney& m);
+
+	void operator=(const EngMoney& m);
+	EngMoney operator+(const EngMoney& m) const;
+	EngMoney operator-(const EngMoney& m) const;
+	EngMoney operator*(const int n) const;
+	EngMoney operator+=(const EngMoney& m);
+	EngMoney operator-=(const EngMoney& m);
+	EngMoney operator*=(const int n);
+	bool operator==(const EngMoney& m) const;
+	bool operator!=(const EngMoney& m) const;
+	bool operator>(const EngMoney& m) const;
+	bool operator>=(const EngMoney& m) const;
+	bool operator<(const EngMoney& m) const;
+	bool operator<=(const EngMoney& m) const;
+	friend ostream& operator<<(ostream& out, const EngMoney& m);
+	friend istream& operator>>(istream& in, EngMoney& m);
+};
